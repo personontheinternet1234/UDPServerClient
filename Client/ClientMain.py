814bfa44256301daa28c6f92684e99863b78266e
Isaac <135486733+personontheinternet1234@users.noreply.github.com>
2024-11-17 23:30:39 -1000

    Update ClientMain.py

--- Client/ClientMain.py
+++ Client/ClientMain.py
@@ -1,9 +1,9 @@
-from Client import Connection
+from Client import ClientConnection
 
 if __name__ == "__main__":
-    server_ip = "192.168.1.15"
-    plane_ip = "192.168.1.7"
+    server_addr = "192.168.1.15"
+    client_addr = "192.168.1.7"
     port = 5559
 
-    planeReceiver = PlaneReceiver(server_ip, plane_ip, port)
-    planeReceiver.start_threads()
+    connection = ClientConnection(server_addr, client_addr, port)
+    connection.start_threads()
