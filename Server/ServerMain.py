4e40d0bca7eea62ed1594bacf6203df15a21a05c
Isaac <135486733+personontheinternet1234@users.noreply.github.com>
2024-11-17 23:29:26 -1000

    Update ServerMain.py

--- Server/ServerMain.py
+++ Server/ServerMain.py
@@ -3,5 +3,6 @@ from Server import ThreadedServer
 if __name__ == "__main__":
     host_addr = "192.168.1.15"
     client_addr = "192.168.1.7"
+    port = 5559
 
-    groundControlServer = ThreadedServer(host_addr, 5559)
+    groundControlServer = ThreadedServer(host_addr, port)
